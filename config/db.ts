fd789b5c008af79196ed047dfe4ed396d412c8af
tawiah <tawiahin4k@gmail.com>
2025-01-10 17:22:02 +0000

    update MongoDB connection string and add dotenv configuration

--- config/db.ts
+++ config/db.ts
@@ -1,10 +1,17 @@
 /** @format */
 
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// const MongoDB_URI = process.env.MONGODB_URI;
 
 export const connectionDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb://localhost:27017/cars`);
+    const conn = await mongoose.connect(
+      `mongodb+srv://tawiahin4k:PrOxyOs35MQBO2Ma@cluster0.7xoac.mongodb.net/cars?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     if (error instanceof Error) {
@@ -15,3 +22,7 @@ export const connectionDB = async () => {
     process.exit(1);
   }
 };
+
+// PrOxyOs35MQBO2Ma
+// tawiahin4k;
+// mongodb+srv://tawiahin4k:PrOxyOs35MQBO2Ma@cluster0.7xoac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
